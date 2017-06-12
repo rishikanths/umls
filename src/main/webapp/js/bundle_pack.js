0af7fb303f099d908de61f99c5b9d65a91e1f575
Rishi Saripalle <rsaripa@ilstu.edu>
2017-06-12 08:40:53 -0500

    Changes to Bundle JS

--- src/main/webapp/js/bundle_pack.js
+++ src/main/webapp/js/bundle_pack.js
@@ -1,3 +1,39 @@
 /**
  * 
  */
+
+var svgPack;
+function packLayout(hierarchyData) {
+
+	var div, format;
+	var pack;
+
+	var width = $("#hierarchy").width();
+	var height = 900;
+
+	$("#hierarchy").empty();
+	div = d3.select("#hierarchy").insert("div", "h2").style("height",
+			height + "px").style("-webkit-backface-visibility", "hidden");
+	svgPack = div.append("svg:svg").attr("width", width).attr("height", height)
+			.append("svg:g")
+			.attr("transform", "translate(" + 2 + "," + 2 + ")");
+	format = d3.format(",d");
+
+	pack = d3.layout.pack().size([ width - 10, width - 10 ]);
+	
+	var root = getHierarchy(hierarchyData.data);
+	var node = svgPack.selectAll(".node").data(root).enter()
+			.append("g").attr("class", function(d) {
+				return d.children ? "node" : "leaf node";
+			}).attr("transform", function(d) {
+				return "translate(" + d.x + "," + d.y + ")";
+			});
+	node.append("title").text(function(d) {
+		return formatName(d.name) + "\n" + format(d.value);
+	});
+	node.append("circle").attr("r", function(d) {
+		return d.r;
+	});
+	node.filter(function(d) {return !d.children;})
+		.append("text").attr("dy", "0.3em").text(function(d) {return d.name.substring(0, d.r / 3);});
+}
